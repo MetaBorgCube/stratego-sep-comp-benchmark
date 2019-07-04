1a1d66ba5f56a6881b60c7a5318daa28dd63e0c6
Jeff Smits <mail@jeffsmits.net>
2019-07-04 17:43:44 +0200

    Use awk, never mind the broken MacOS awk

--- docker/preprocess.sh
+++ docker/preprocess.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 $(dirname $0)/imports.sh
-find . -name '*.str' | xargs gawk -i inplace -f $(dirname $0)/nullary-constructors.awk
+find . -name '*.str' | xargs awk -i inplace -f $(dirname $0)/nullary-constructors.awk
