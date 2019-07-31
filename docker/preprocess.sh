7e62ac561370e2517309464f3e33120aa9043bf8
Jeff Smits <mail@jeffsmits.net>
2019-07-31 16:19:47 +0200

    Add 1 second sleep in preprocessing so Makefile.am change is actually picked up

--- docker/preprocess.sh
+++ docker/preprocess.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
-sed -i'' -e 's/sdfdata_DATA =/sdfdata_DATA :/' Makefile.am && make sdfdata_DATA
+sed -i'' -e 's/sdfdata_DATA =/sdfdata_DATA :/' Makefile.am
+sleep 1
+make sdfdata_DATA
 $(dirname $0)/imports.sh
 find . -name '*.str' | xargs gawk -i inplace -f $(dirname $0)/nullary-constructors.awk
