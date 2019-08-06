86364606b04a8d6072d860cc4d68c348dc5e2fb4
Jeff Smits <mail@jeffsmits.net>
2019-08-06 15:06:54 +0200

    Force clean build of the table files, it seems to break on commit f401375 of webdsl otherwise

--- docker/preprocess.sh
+++ docker/preprocess.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 set -euv
 sed -i'' -e 's/sdfdata_DATA =/sdfdata_DATA :/' Makefile.am
+make fullclean 2>&1
+cp share/strategoxt/strategoxt/strategoxt.jar .
 make sdfdata_DATA 2>&1
 DIR=$(dirname $0)
 $DIR/imports.sh
